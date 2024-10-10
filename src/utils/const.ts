f7fd83879b2802f4ba12849b7a5e0cb4607fef99
1shaked <40497915+1shaked@users.noreply.github.com>
2024-10-10 16:23:14 +0300

    refactor: move regions data to a separate constants file

---
+++ src/utils/const.ts
@@ -0,0 +1,22 @@
+export const regions = {
+    north: {
+      longitude: 35.2033,
+      latitude: 33.095,
+      altitude: 100000,
+    },
+    south: {
+      longitude: 34.7913,
+      latitude: 29.5581,
+      altitude: 100000,
+    },
+    east: {
+      longitude: 35.5735,
+      latitude: 31.0461,
+      altitude: 100000,
+    },
+    west: {
+      longitude: 34.3289,
+      latitude: 31.0461,
+      altitude: 100000,
+    },
+  } as const;

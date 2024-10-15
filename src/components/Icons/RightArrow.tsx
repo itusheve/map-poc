016c960b4d1bf41a36cd5f71ecc0fa7dd7d52261
1shaked <40497915+1shaked@users.noreply.github.com>
2024-10-15 09:38:00 +0300

    feat: add TreeLinesHamburgerSVG component and update RightArrowSvg for improved structure

--- src/components/Icons/RightArrow.tsx
+++ src/components/Icons/RightArrow.tsx
@@ -2,7 +2,9 @@ import { IconPropsI } from "./types";
 
 
 export function RightArrowSvg(props: IconPropsI) {
-    return <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" className={props.className} style={props.style} onClick={() => props.onClick?.()}>
+    return <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg" 
+    className={props.className} style={props.style} onClick={() => props.onClick?.()}
+    >
         <path d="M6 17.4142L11 12.4142L6 7.41418" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
         <path d="M13 17.4142L18 12.4142L13 7.41418" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
